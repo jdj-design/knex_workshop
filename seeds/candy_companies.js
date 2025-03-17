da3e7339f7d49c2293f020e13c0f8e11c4f69374
jdj-design <doc_jones12@yahoo.com>
2025-03-17 12:56:25 -0400

    added company name seed

---
+++ seeds/candy_companies.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('candy_companies').del()
+  await knex('candy_companies').insert([
+    {id: 1, name: 'Hershey', location: 'USA'},
+    {id: 2, name: 'Mars', location: 'USA'},
+    {id: 3, name: 'Haribo', location: 'Germany'},
+    {id: 4, name: 'Morinaga & Co.', location: 'Japan'}
+  ]);
+};
